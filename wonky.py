225691a28a34fe45638612b8f1339058394cfb0e
Tadhg O'Brien <tigger04@gmail.com>
2023-02-14 17:58:15 +0000

    tiggers-Air

--- wonky.py
+++ wonky.py
@@ -122,7 +122,8 @@ class Window(QWidget,):
         op.setOpacity(1) #0 to 1 will cause the fade effect to kick in
         self.setGraphicsEffect(op)
         
-        flags = QtCore.Qt.WindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnBottomHint | QtCore.Qt.CustomizeWindowHint | QtCore.Qt.BypassWindowManagerHint)
+        # flags = QtCore.Qt.WindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnBottomHint | QtCore.Qt.CustomizeWindowHint | QtCore.Qt.BypassWindowManagerHint)
+        flags = QtCore.Qt.WindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnBottomHint | QtCore.Qt.CustomizeWindowHint )
 
         # flags = QtCore.Qt.WindowFlags(QtCore.Qt.BypassWindowManagerHint)
 
